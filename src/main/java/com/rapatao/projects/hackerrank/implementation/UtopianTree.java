555d6c2ea0970514593848de446539cdb44d8d28
Luiz Henrique Rapatao <rapatao@rapatao.com>
2016-05-12 13:18:32 -0300

    add solution for Utopian Tree

--- src/main/java/com/rapatao/projects/hackerrank/implementation/UtopianTree.java
+++ src/main/java/com/rapatao/projects/hackerrank/implementation/UtopianTree.java
@@ -15,12 +15,23 @@ public class UtopianTree {
     public void execute(final String[] args) {
         int tests = nextInt();
         for (int i = 0; i < tests; i++) {
-            throw new RuntimeException("to be implemented!");
+            int cycles = nextInt();
+
+            int height = 1;
+            for (int c = 0; c < cycles; c++) {
+                height = (c % 2 == 0) ? height * 2 : height + 1;
+            }
+            print(height);
+
         }
     }
 
     public int nextInt() {
         return scanner.nextInt();
+    }
+
+    public void print(final int result) {
+        System.out.println(result);
     }
 
 }
---
+++ src/test/java/com/rapatao/projects/hackerrank/implementation/UtopianTreeTest.java
@@ -0,0 +1,25 @@
+package com.rapatao.projects.hackerrank.implementation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * @author Luiz Henrique Rapatao - rapatao@rapatao.com
+ * @since 12/05/2016
+ */
+public class UtopianTreeTest {
+
+    private UtopianTree mock = Mockito.spy(new UtopianTree());
+
+    @Before
+    public void setUp() {
+        Mockito.doReturn(3).doReturn(0).doReturn(1).doReturn(4).when(mock).nextInt();
+    }
+
+    @Test
+    public void test() {
+        mock.execute(null);
+    }
+
+}
