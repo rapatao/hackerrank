5506fd14ea269252aabefced793a036f79b662b5
Luiz Henrique Rapatao <rapatao@rapatao.com>
2016-05-12 15:27:38 -0300

    Intro to Tutorial Challenges

---
+++ src/main/java/com/rapatao/projects/hackerrank/sorting/IntroToTutorialChallenges.java
@@ -0,0 +1,31 @@
+package com.rapatao.projects.hackerrank.sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Luiz Henrique Rapatao - rapatao@rapatao.com
+ * @since 12/05/2016
+ */
+public class IntroToTutorialChallenges {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void execute(final String[] args) {
+        int v = nextInt();
+        int n = nextInt();
+        int[] ar = new int[n];
+        ar = Arrays.stream(ar).map(p -> nextInt()).sorted().toArray();
+        final int pos = Arrays.binarySearch(ar, v);
+        print(pos);
+    }
+
+    public int nextInt() {
+        return scanner.nextInt();
+    }
+
+    public void print(final int result) {
+        System.out.println(result);
+    }
+
+}
