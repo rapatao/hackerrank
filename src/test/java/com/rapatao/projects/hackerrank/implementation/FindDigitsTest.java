8ce9ec586fef2f65791ec81a76f208a2f556a222
Luiz Henrique Rapatao <rapatao@rapatao.com>
2016-05-12 15:01:55 -0300

    Find Digits

---
+++ src/test/java/com/rapatao/projects/hackerrank/implementation/FindDigitsTest.java
@@ -0,0 +1,25 @@
+package com.rapatao.projects.hackerrank.implementation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * @author Luiz Henrique Rapatao - rapatao@rapatao.com
+ * @since 12/05/2016
+ */
+public class FindDigitsTest {
+
+    private FindDigits mock = Mockito.spy(new FindDigits());
+
+    @Before
+    public void setUp() {
+        Mockito.doReturn(2).doReturn(12).doReturn(1012).when(mock).nextInt();
+    }
+
+    @Test
+    public void test() {
+        mock.execute(null);
+    }
+
+}
